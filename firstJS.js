37970fd65b116ee8a0de00b6eb81cec03981b0d8
Kateryna Danylo <104447160+KatDanylo@users.noreply.github.com>
2022-05-19 18:14:55 +0300

    Update firstJS.js

--- firstJS.js
+++ firstJS.js
@@ -1,1 +1,27 @@
+// https://www.codewars.com/kata/544675c6f971f7399a000e79
+const stringToNumber = function (str) {
+  return +str;
+};
+
+// https://www.codewars.com/kata/55685cd7ad70877c23000102
+function makeNegative(num) {
+  if (num < 0) {
+    return num;
+  } else {
+    return -num;
+  }
+}
+
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+let goals = (lL, cDr, cLg) => lL + cDr + cLg;
+
+// https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+function greet(name, owner) {
+  return name == owner ? "Hello boss" : "Hello guest";
+}
+
+// https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
 
